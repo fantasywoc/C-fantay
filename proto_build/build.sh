a7f6a3d8b65b4c1fa818c8048b946adf944b6653
80046361 <cuikunfeng@hozonauto.com>
2024-07-08 12:57:40 +0800

    add proto_build

---
+++ proto_build/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash  
+  
+current_dir=$(pwd) 
+
+echo "当前目录是: $current_dir"
+
+export LD_LIBRARY_PATH=$current_dir/protobuf/lib:$LD_LIBRARY_PATH
+
+
+rm -rf build && mkdir build && cd build && cmake .. && make
