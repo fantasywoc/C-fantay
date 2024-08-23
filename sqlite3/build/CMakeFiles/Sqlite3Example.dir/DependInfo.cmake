a1c7a80e0c33dd3dcc1abf850a25ff96d1677519
15238469518@163.com <15238469518@163.com>
2024-08-23 15:50:57 +0800

    update sqlite3

---
+++ sqlite3/build/CMakeFiles/Sqlite3Example.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/kf/data/kf/C-fantay/sqlite3/src/main.cpp" "/media/kf/data/kf/C-fantay/sqlite3/build/CMakeFiles/Sqlite3Example.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/f/data/sqlite3/include"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
